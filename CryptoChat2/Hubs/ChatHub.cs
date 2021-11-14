138fa333a0b26ceda68bee5bd75ac1662bf590de
Krzysztof Stachowicz <Gipad19@gmail.com>
2021-11-14 11:28:59 +0100

    Dodaj pliki projektów.

---
+++ CryptoChat2/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CryptoChat2.Hubs
+{
+    public class ChatHub :Hub
+    {
+        public async Task SendMessage(string username, string message)
+        {
+            Clients.All.SendAsync("ReceiveMessage", username, message);
+        }
+    }
+}
